e2ae915c49f7b51f574a4abb3a13d92e321a1ac3
liuba-larousse <liubovkapitulskaya@gmail.com>
2020-08-06 12:31:58 -0500

    01-city-tours init files

---
+++ 01-city-tours/src/components/cards.jsx
@@ -0,0 +1,49 @@
+
+
+const cards =[
+    {
+        id: 0,
+        name: "Mountains",
+        tour: "Sky Tour",
+        src: "https://images.unsplash.com/photo-1571897091477-76aa55bd36d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=984&q=80",
+        description: "A mountain is a large landform that rises above the surrounding land in a limited area, usually in the form of a peak"
+    },
+    {
+        id: 1,
+        name: "Ocean",
+        tour: "Serenety Tour",
+        src: "https://images.unsplash.com/photo-1570320977701-df924af001db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80",
+        description: "An ocean is a body of water that composes much of a planet's hydrosphere."
+    },
+    {
+        id: 2,
+        name: "City",
+        tour: "Night Lights Tour",
+        src: "https://images.unsplash.com/photo-1573674095735-844c07dd3747?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+        description: "A city is a large human settlement.It can be defined as a permanent and densely settled place with administratively defined boundaries whose members work primarily on non-agricultural tasks."
+    },
+    {
+    id: 3,
+        name: "Village",
+        tour: "Into The Wild Tour",
+        src: "https://images.unsplash.com/photo-1517821099606-cef63a9bcda6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1096&q=80",
+        description: "A village is a clustered human settlement or community, larger than a hamlet but smaller than a town , with a population typically ranging from a few hundred to a few thousand. "
+    },
+    {
+        id: 4,
+            name: "Space",
+            tour: "Stars Tour",
+            src: "https://images.unsplash.com/photo-1532026525415-cf16e856b61d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=972&q=80",
+            description: "Space is the boundless three-dimensional extent in which objects and events have relative position and direction. "
+        },
+        {
+            id: 5,
+                name: "Desert",
+                tour: "Sandy Winds Tour",
+                src: "https://images.unsplash.com/photo-1539452146933-466b959d1019?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+                description: "A desert is a barren area of landscape where little precipitation occurs and, consequently, living conditions are hostile for plant and animal life."
+            }
+]
+
+
+export default cards;
